f09749af14c2c1c4bd9184734ff08e17ee0730c7
madyanes <madyanes.dev@gmail.com>
2023-09-02 00:36:15 +0700

    building functionality with classes

---
+++ features/classes.ts
@@ -0,0 +1,29 @@
+class Vehicle {
+  constructor(public color: string) {}
+
+  protected honk(): void {
+    console.log('beep');
+  }
+}
+
+const vehicle = new Vehicle('orange');
+console.log(vehicle.color);
+
+class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    // be careful! if you put `public` modifier at `color`, it won't belong to parent class
+    super(color);
+  }
+
+  private drive(): void {
+    console.log('vroom');
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
+  }
+}
+
+const car = new Car(4, 'red');
+car.startDrivingProcess();
